460ed45eb07e68bf6699d7ef7cc6ac3305ad7291
mrc <marcoemorales2015@gmail.com>
2023-05-26 23:38:27 -0300

    Tasks and Tasks Rooms (models without hash)

---
+++ tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Task(models.Model):
+    def __str__(self):
+        return -1
+
+
+class TasksRoom(models.Model):
+    def __str__(self):
+        return -1
