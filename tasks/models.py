24a0419ee85cc0505d6465ed1dda01daef6167ad
mrc <marcoemorales2015@gmail.com>
2023-05-27 23:59:32 -0300

    Tasks and Tasks Rooms (without relations)

--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,25 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Task(models.Model):
-    def __str__(self):
-        return -1
-
-
 class TasksRoom(models.Model):
+    id_room = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    title = models.CharField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} | {self.id_room}"
+
+
+class Task(models.Model):
+    title = models.CharField(
+        required=True,
+        blank=False,
+    )
+    description = models.CharField()
+    room = models.ForeignKey(TasksRoom, on_delete=models.CASCADE, blank=False)
+
     def __str__(self):
         return -1
