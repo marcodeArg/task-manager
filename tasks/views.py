f1b1117afca14ea0aef29934a19347bae3417af9
mrc <marcoemorales2015@gmail.com>
2023-05-29 23:58:59 -0300

    html for the main content

--- tasks/views.py
+++ tasks/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, "base.html")
+    return render(request, "home.html")
