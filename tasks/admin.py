b0ff6c3dd0e15fbd3a31c86b681d8a53d47323b6
mrc <marcoemorales2015@gmail.com>
2023-06-03 23:55:53 -0300

    Home with functionality enter/create room done

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import TasksRoom, Task
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 
 admin.site.register(TasksRoom)
 admin.site.register(Task)
+
+
+class TasksRoomInline(admin.TabularInline):
+    model = TasksRoom.users.through
+    extra = 0
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [TasksRoomInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,52 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import TaskRoomForm
+from django.contrib import messages
+from .models import TasksRoom, Task
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.shortcuts import HttpResponse
 
 
 # Create your views here.
 @login_required
 def home(request):
+    context = {}
+
     formRoom = TaskRoomForm()
 
     if request.method == "POST":
+        # Button CREATE
         if "create" in request.POST:
+            formRoom = TaskRoomForm(request.POST)
             if formRoom.is_valid():
-                pass
+                form_instance = formRoom.save(commit=True)
+                form_instance.users.add(request.user)
 
-    return render(request, "home.html", {"formRoom": formRoom})
+                room_hash = form_instance.id_room
+                redirect_url = reverse("room", kwargs={"hash": room_hash})
+
+                messages.success(request, "Room created successfully")
+                return redirect(redirect_url)
+            else:
+                messages.error(request, "A error occurred while creating")
+        # Button ENTER
+        else:
+            room_hash = request.POST["room_hash"]
+            if TasksRoom.objects.filter(id_room=room_hash).exists():
+                room = TasksRoom.objects.get(id_room=room_hash)
+                room.users.add(request.user)
+                messages.success(request, "You entered the room successfully!")
+                redirect_url = reverse("room", kwargs={"hash": room_hash})
+                return redirect(redirect_url)
+            else:
+                messages.error(request, "A error occurred while entering")
+
+    context["formRoom"] = formRoom
+    rooms = TasksRoom.objects.filter(users=request.user)
+    context["rooms"] = rooms
+    return render(request, "home.html", context)
+
+
+def room(request, hash):
+    return HttpResponse("Romm id: " + str(hash))
