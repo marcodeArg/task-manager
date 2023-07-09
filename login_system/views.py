cb360ad7cef6d1023cdb1406e73b225ef4d870ea
mrc <marcoemorales2015@gmail.com>
2023-07-09 19:10:54 -0300

    Login system (remove unnecessary prints)

--- login_system/views.py
+++ login_system/views.py
@@ -36,12 +36,8 @@ def signin(request):
 
     if request.method == "POST":
         form = SignInForm(request.POST)
-        print(request.POST)
-        print()
-        print(form)
-        if form.is_valid():
-            print(form.cleaned_data)
 
+        if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password1"]
 
